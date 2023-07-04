60d113eb1e4d80114322afc9bf4ba1983373ffbf
Bryan Martínez <bryan.martinez@tractozone.com.mx>
2023-07-03 18:34:38 -0600

    Carga de template

---
+++ templates/bundle_footer.php
@@ -0,0 +1,7 @@
+    <!-- Vendor scripts: js libraries and plugins-->
+    <script src="assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js"></script>
+    <!-- Main theme script-->
+    <script src="assets/js/theme.min.js"></script>
+    <!-- Customizer-->
+    
